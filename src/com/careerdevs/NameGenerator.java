2c769ba62351316497e46d930107a0c055ffd8c5
christine <chrisitne.brown@bhcc.edu>
2022-01-04 21:04:03 -0500

    add part 1 and 2 of basic java

---
+++ src/com/careerdevs/NameGenerator.java
@@ -0,0 +1,12 @@
+package com.careerdevs;
+
+public class NameGenerator {
+   public  String generateFullName(String param1, String param2){
+       return param1 + " " + param2;
+
+    }
+
+}
+
+//NON STATIC USES FIELDS create new instance
+//STATIC USES PARameters, access with dot notation
