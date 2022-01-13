5d8de6a78d21f740168d2e2565e0a6bb0fe8192f
christine <chrisitne.brown@bhcc.edu>
2022-01-13 00:28:19 -0500

    add part 1 and 2 of basic java

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,15 @@
+package com.careerdevs;
+
+public class Car {
+    public String makes;
+    public String model;
+    public int mileage;
+    public float gasTankPercent;
+
+    public Car( String a , String b , int c, float d ){
+        makes = a;
+        model = b;
+        mileage = c;
+        gasTankPercent = d;
+    }
+}
--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -13,8 +13,19 @@ public class Main {
         System.out.println(name);
 
     NameGenerator printName = new NameGenerator();
-       System.out.print(printName.generateFullName("chrstine" , "brown"));
+       System.out.println(printName.generateFullName("chrstine" , "brown"));
        //NameGenerator.generateFullName()
+
+        Car myCar = new Car("taurus" , "ford", 1452, 30);
+        System.out.println("Make:" + " " + myCar.makes + " Model " + myCar.model + " " + "mileage " + " "+ myCar.mileage + " " + "gasTankPercent " + " " + myCar.gasTankPercent) ;
+
+        Car myCar1 = new Car("mercury" , "saturn", 18852, 20);
+        System.out.println("Make:" + " " + myCar1.makes + " Model " + myCar1.model + " " + "mileage " + " "+ myCar1.mileage + " " + "gasTankPercent " + " " + myCar1.gasTankPercent) ;
+
+        Car myCar2 = new Car("intrepid" , "dodge", 188542, 70);
+        System.out.println("Make:" + " " + myCar2.makes + " Model " + myCar2.model + " " + "mileage " + " "+ myCar2.mileage + " " + "gasTankPercent " + " " + myCar2.gasTankPercent) ;
+
+
 
 
     }
