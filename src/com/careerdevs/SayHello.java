faf7cc9b8a01bb7ba715d1255ce16186f21728f2
christine <chrisitne.brown@bhcc.edu>
2022-01-16 20:47:27 -0500

    completed basic java challenge say hello challenge

---
+++ src/com/careerdevs/SayHello.java
@@ -0,0 +1,32 @@
+package com.careerdevs;
+
+import java.util.Scanner;
+
+public class SayHello {
+    public static void  whatIsYourName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name");
+        String name = scanner.next();
+        System.out.println("Hello," + " " + name + " nice to meet you");
+    };
+
+    public static void whatIsYourFullName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your first name");
+        String fName = scanner.next();
+        System.out.println("What is your last name");
+        String lName = scanner.next();
+        System.out.println("It is so nice to formally meet you " + fName + " " + lName);
+
+
+    }
+
+    public static void main(String[] args) {
+        whatIsYourName();
+        whatIsYourFullName();
+
+
+
+    }
+
+}
