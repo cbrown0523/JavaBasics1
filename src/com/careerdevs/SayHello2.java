50f94b0883afabffc6ba05bd5ed3036a6a5e0ee2
christine <chrisitne.brown@bhcc.edu>
2022-01-16 21:26:33 -0500

    completed basic java challenge say hello2 challenge

---
+++ src/com/careerdevs/SayHello2.java
@@ -0,0 +1,50 @@
+package com.careerdevs;
+
+import java.util.Scanner;
+
+public class SayHello2 {
+    public static void whatIsYourName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name");
+        String name = scanner.next();
+        System.out.println("Hello," + " " + name + " nice to meet you");
+
+    }
+        public static void whatIsYourFullName () {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What is your first name");
+            String fName = scanner.next();
+            System.out.println("What is your last name");
+            String lName = scanner.next();
+            System.out.println("It is so nice to formally meet you " + fName + " " + lName);
+
+        }
+    public static void whereBorn () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What city were you born in");
+        String city = scanner.next();
+        System.out.println("What year were you born in");
+        int year = scanner.nextInt();
+        System.out.println(city + " " + "is a nice city" + year + " " + " was a great year");
+
+    }
+
+    public static void favNum() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your favorite number");
+        int num = scanner.nextInt();
+        System.out.println(num + ' ' + "is your lucky number!");
+
+    }
+
+        public static void main (String[]args){
+            whatIsYourName();
+            whatIsYourFullName();
+            whereBorn();
+            favNum();
+
+
+        }
+
+    }
+
