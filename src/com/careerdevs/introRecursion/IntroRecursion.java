42f1be2c42b38d0763a9a69e77661bee13eb95ad
christine <chrisitne.brown@bhcc.edu>
2022-02-27 01:19:17 -0500

    intro recursion

---
+++ src/com/careerdevs/introRecursion/IntroRecursion.java
@@ -0,0 +1,13 @@
+package com.careerdevs.introRecursion;
+
+public class IntroRecursion {
+        public static int sum(int n) {
+            if(n==0)
+                return 0;
+            else if(n==1)
+                return 1;
+            else
+                return n+sum(n-1);
+
+    }
+}
